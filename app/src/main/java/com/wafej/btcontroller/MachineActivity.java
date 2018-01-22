9737813d0f5515df206541bddd9503ba11a89a23
wangfeng <1401617948@qq.com>
2018-01-22 21:42:58 +0800

    machine&controller UI

---
+++ app/src/main/java/com/wafej/btcontroller/MachineActivity.java
@@ -0,0 +1,16 @@
+package com.wafej.btcontroller;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by wafej on 2018/1/22.
+ */
+
+public class MachineActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_machine);
+    }
+}
--- app/src/main/java/com/wafej/btcontroller/MainActivity.java
+++ app/src/main/java/com/wafej/btcontroller/MainActivity.java
@@ -1,14 +1,46 @@
 package com.wafej.btcontroller;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener{
+
+    private Button mBtnMachine = null;
+    private Button mBtnController = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        mBtnMachine = (Button) findViewById(R.id.btn_machine);
+        mBtnController = (Button) findViewById(R.id.btn_controller);
+
+        mBtnMachine.setOnClickListener(this);
+        mBtnController.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()){
+            case R.id.btn_machine:
+                Intent intentMachine = new Intent(MainActivity.this, MachineActivity.class);
+                intentMachine.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intentMachine);
+                finish();
+                break;
+            case R.id.btn_controller:
+                Intent intentContoller = new Intent(MainActivity.this, ControllerActivity.class);
+                intentContoller.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(intentContoller);
+                finish();
+                break;
+            default:
+                break;
+        }
     }
 }
